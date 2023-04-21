f32806fbe901390a3b5d5ed6fb60eb325b8af13e
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-21 00:30:21 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    ssize_t len = sizeof(MESSAGE) - 1;
+    ssize_t written = write(2, MESSAGE, len);
+    return (written == len) ? 1 : -1;
 }
