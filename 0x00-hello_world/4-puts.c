f1f62394d1ad36c022efe708912a5d81dd791924
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-21 00:13:33 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-gcc -Wall -Werror -Wextra -pedantic program.c -o program;
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
