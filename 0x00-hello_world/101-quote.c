7a2cfb4af247a313a43058c431b1ad8306267223
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-21 00:38:58 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,5 +9,5 @@ int main(void)
 {
     const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(STDERR_FILENO, msg, strlen(msg));
-    return 1;
+    return (1);
 }
