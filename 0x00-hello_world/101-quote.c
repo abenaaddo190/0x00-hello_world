8043f374779cf87850173f67cca14f77497e84ea
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-21 00:49:44 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-#define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-    ssize_t len = sizeof(MESSAGE) - 1;
-    ssize_t written = write(2, MESSAGE, len);
-    if(written == len)
-    {
-        return(1);
-    }
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
