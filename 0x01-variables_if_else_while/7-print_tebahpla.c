9bbcb8ee5ea0b440cc19c674fb733d404216b3c8
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-22 00:21:32 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,19 @@
 
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	
+	putchar('\n');
+	
 	return (0);
 }
