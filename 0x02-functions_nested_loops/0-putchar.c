bb5fb619413e14ca638cf2dbf27e3fa3e1a42095
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-25 23:07:17 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,9 +8,10 @@ int main(void)
 {
     char s[] = "_putchar\n";
     int i;
+    
     for (i = 0; s[i] != '\0'; i++)
     {
-        _putchar(s[i]);
+            _putchar(s[i]);
     }
     return (0);
 }
