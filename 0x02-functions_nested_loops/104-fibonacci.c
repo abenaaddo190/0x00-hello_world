541f3661be5c19b73fb60ec5d1cb13eaf41fd377
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-26 01:18:27 +0000

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* Output
+*/
+
+int main(void)
+{
+int prev = 1;
+int current = 2;
+int count = 0;
+
+printf("%d, %d, ", prev, current);
+count += 2;
+
+while (count < 98)
+{
+	int next = prev + current;
+	printf("%d", next);
+	
+	if (count < 97)
+	{
+		printf(", ");
+	}
+	prev = current;
+	current = next;
+	count++;
+}
+
+    printf("\n");
+
+    return 0;
+}
