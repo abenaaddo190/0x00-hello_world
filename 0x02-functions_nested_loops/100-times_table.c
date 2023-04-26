ebc921f2ccbff2d2f9013f628d2d36c36424310e
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-04-26 00:27:18 +0000

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,16 +16,16 @@ if (n >= 0 && n <= 15)
 		for (j = 0; j <= n; j++)
 		{
 			product = i * j;
-				if (j == 0)
-					printf("%d", product);
-				else if (product < 10)
-                    printf("   %d", product);
-                else if (product < 100)
-                    printf("  %d", product);
-                else
-                    printf(" %d", product);
-                if (j != n)
-                    printf(",");
+			if (j == 0)
+				printf("%d", product);
+			else if (product < 10)
+				printf("   %d", product);
+			else if (product < 100)
+				printf("  %d", product);
+			else
+				printf(" %d", product);
+			if (j != n)
+				printf(",");
 		}
 		printf("\n");
 	}
