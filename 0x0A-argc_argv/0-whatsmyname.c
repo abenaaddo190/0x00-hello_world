fb2df5def1052250838ebf36235ce32ece4ed825
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-05-12 21:10:33 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,14 +7,9 @@
  * @argv: array of arguments
  * Return: 0 if successful
  */
-
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc ; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-	return (0);
+    (void)argc;
+    printf("%s\n", argv[0]);
+    return (0);
 }
